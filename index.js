f849f83d72d4bd1fac443b2b5767cabb97bfaf47
Typescript Expert" ,Beginning of Generative AI journey through initiative of Governor Sindh👩🏻‍💻 | CSS | HTML | <161370938+hunzaali123456@users.noreply.github.com>
2024-03-26 06:11:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randamNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6 :",
+    },
+]);
+if (answers.userGuessedNumber === randamNumber) {
+    console.log("congratulations! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
